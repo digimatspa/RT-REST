0f18bb6b81d7a5ce3d9c6fb042188374cdf3c3c9
Felix Kronlage <kronlage@bytemine.net>
2015-03-27 21:44:18 +0100

    added comment, attachment, history, user; reimplemented parser; added BASIC auth client
        (cherry picked from commit fda845e02cb74e7b0deefde1ee0212ea3360e9a3)
    
    adapt RESTRTTicketDAO.java to these changes.

--- src/main/java/de/boksa/rt/rest/RTRESTClient.java
+++ src/main/java/de/boksa/rt/rest/RTRESTClient.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2012  Benjamin Boksa (http://www.boksa.de/)
- * 
+ *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
  *   You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *   Unless required by applicable law or agreed to in writing, software
  *   distributed under the License is distributed on an "AS IS" BASIS,
  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,27 +18,27 @@ package de.boksa.rt.rest;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpHost;
 import org.apache.http.Consts;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.params.ClientPNames;
 import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
+
+import de.boksa.rt.model.RTTicket;
 
 public class RTRESTClient {
-	
+
 	public enum TicketSearchResponseFormat {
 		IDONLY("i"),
 		IDANDSUBJECT("s"),
@@ -55,7 +55,7 @@ public class RTRESTClient {
 		}
 	}
 	
-	private static final Pattern PATTERN_RESPONSE_BODY = Pattern.compile("^(.*) (\\d+) (.*)\n((.*\n)*+)", Pattern.MULTILINE);
+	private static final Pattern PATTERN_RESPONSE_BODY = Pattern.compile("^(.*) (\\d+) (.*)\n((.*\n)*)", Pattern.MULTILINE);
 	private static final String NO_MATCHING_RESULTS = "No matching results";
 		
 	private String restInterfaceBaseURL;
@@ -66,7 +66,6 @@ public class RTRESTClient {
 	private BasicHttpContext httpContext;
 	
 	public RTRESTClient(String restInterfaceBaseURL, String username, String password) {
-		this.httpContext = new BasicHttpContext();
 		this.setRestInterfaceBaseURL(restInterfaceBaseURL);
 		this.setUsername(username);
 		this.setPassword(password);
@@ -94,24 +93,25 @@ public class RTRESTClient {
 
 	public RTRESTResponse searchTickets(String query, String orderby) throws IOException {
 		return this.searchTickets(query, orderby, null);
-	}		
+	}
 
 	public RTRESTResponse searchTickets(String query, TicketSearchResponseFormat format) throws IOException {
 		return this.searchTickets(query, null, format);
-	}		
-	
-	public RTRESTResponse searchTickets(String query, String orderby, TicketSearchResponseFormat format) throws IOException {
+	}
+
+	public RTRESTResponse searchTickets(String query, String orderby, TicketSearchResponseFormat format)
+			throws IOException {
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
-		params.add(new BasicNameValuePair("query", query));		
-		
+		params.add(new BasicNameValuePair("query", query));
+
 		if (orderby != null) {
 			params.add(new BasicNameValuePair("orderby", orderby));
 		}
-		
+
 		if (format != null) {
 			params.add(new BasicNameValuePair("format", format.getFormatString()));
 		}
-		
+
 		return this.getResponse("search/ticket", params);
 	}
 
@@ -122,40 +122,107 @@ public class RTRESTClient {
 		return this.getResponse("ticket/"+ ticketId +"/edit", params);
 	}
 
+	public RTRESTResponse getTicket(Long ticketId) throws IOException {
+		String url = "ticket/" + ticketId;
+		return this.getResponse(url);
+	}
+
+	public RTRESTResponse getTicket(Long ticketId, String attribute) throws IOException {
+		String url = "ticket/" + ticketId + "/" + attribute;
+
+		return this.getResponse(url);
+	}
+
+	public RTRESTResponse getTicket(Long ticketId, String attribute, Long attributeId) throws IOException {
+		String url = "ticket/" + ticketId + "/" + attribute + "/" + attributeId;
+
+		return this.getResponse(url);
+	}
+
+	public RTRESTResponse newTicket(RTTicket ticket, String text) throws IOException {
+		String url = "ticket/new";
+
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		String content = ticket.getNewTicketParams() + "\ntext: " + text;
+		params.add(new BasicNameValuePair("content", content));
+
+		return this.getResponse(url, params);
+	}
+
+	public RTRESTResponse commentOnTicket(RTTicket ticket, Map<String, String> parameters) throws IOException {
+		String url = "ticket/" + ticket.getId() + "/comment";
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		String content = "id: " + ticket.getId() + "\nAction: " + parameters.get("action") + "\nText: "
+				+ parameters.get("text") + "\nAttachment: " + ""; // TODO
+
+		if (parameters.get("action").equals("correspond")) {
+			content += "\nCc: " + parameters.get("cc") + "\nBcc: " + parameters.get("bcc") + "\nTimeWorked: "
+					+ parameters.get("timeworked");
+		}
+		params.add(new BasicNameValuePair("content", content));
+
+		return this.getResponse(url, params);
+	}
+
+	public RTRESTResponse editTicket(RTTicket ticket, Map<String, String> updatedValues) throws IOException {
+		String url = "ticket/" + ticket.getId() + "/edit";
+
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		String content = "";
+
+		for (String key : updatedValues.keySet()) {
+			content += key + ": " + updatedValues.get(key);
+		}
+
+		params.add(new BasicNameValuePair("content", content));
+
+		return this.getResponse(url, params);
+	}
+
+	public RTRESTResponse getUser(String username) throws IOException {
+		String url = "user/" + username;
+		return this.getResponse(url);
+	}
+
+	public RTRESTResponse getUser(Long user_id) throws IOException {
+		String url = "user/" + user_id;
+		return this.getResponse(url);
+	}
+
 	private RTRESTResponse getResponse(String url) throws IOException {
 		return this.getResponse(url, new ArrayList<NameValuePair>());
 	}
-	
+
+	protected HttpContext getHttpContext(HttpHost targetHost) {
+		return new BasicHttpContext();
+	}
+
 	private RTRESTResponse getResponse(String url, List<NameValuePair> params) throws IOException {
 		HttpPost postRequest = new HttpPost(this.getRestInterfaceBaseURL() + url);
-		UrlEncodedFormEntity postEntity = new UrlEncodedFormEntity(params, Consts.UTF_8);
-		postEntity.setContentType("application/x-www-form-urlencoded");
-        	postRequest.setEntity(postEntity);
-        
-		// Explicitly enable handling of authentication automatically
-		this.httpClient.getParams().setParameter(ClientPNames.HANDLE_AUTHENTICATION, true);
-		// Set credentials for the given host:port in advance - the client will use them
-		//  automatically when the server will require authentication
-		this.httpClient.getCredentialsProvider().setCredentials(
-			new AuthScope (postRequest.getURI().getHost(), postRequest.getURI().getPort()), 
-			new UsernamePasswordCredentials(this.getUsername(), this.getPassword()));
 
-		HttpResponse httpResponse = this.httpClient.execute(postRequest);
-		
-		String responseBody = IOUtils.toString(httpResponse.getEntity().getContent(), Consts.UTF_8);
-				
+		UrlEncodedFormEntity postEntity = new UrlEncodedFormEntity(params, HTTP.UTF_8);
+		postEntity.setContentType("application/x-www-form-urlencoded");
+
+		postRequest.setEntity(postEntity);
+
+		HttpContext context = getHttpContext(new HttpHost(postRequest.getURI().getHost(), postRequest.getURI().getPort()));
+
+		HttpResponse httpResponse = this.httpClient.execute(postRequest, context);
+
+		String responseBody = IOUtils.toString(httpResponse.getEntity().getContent(), HTTP.UTF_8);
+
 		Matcher matcher = PATTERN_RESPONSE_BODY.matcher(responseBody);
-		
+
 		RTRESTResponse response = new RTRESTResponse();
 		if (matcher.matches()) {
 			String body = matcher.group(4).trim();
 			// Check if response is 'No matching results'
-			if (body.startsWith (NO_MATCHING_RESULTS)) {
+			if (body.startsWith(NO_MATCHING_RESULTS)) {
 				// Signal upper layers of no records are available by setting
 				// response code to -1 and body to actual response from
 				// endpoint
-				response.setStatusCode (-1l);
-				response.setStatusMessage (matcher.group(4).trim());
+				response.setStatusCode(-1l);
+				response.setStatusMessage(matcher.group(4).trim());
 				return response;
 			}
 			response.setVersion(matcher.group(1));
@@ -166,8 +233,8 @@ public class RTRESTClient {
 		} else {
 			// Pattern didn't match - signal upper layers by setting response
 			// code to -1
-			response.setStatusCode (-1l);
-			response.setStatusMessage ("Response body contents - no match");
+			response.setStatusCode(-1l);
+			response.setStatusMessage("Response body contents - no match");
 		}
 		return response;
 	}
@@ -176,19 +243,25 @@ public class RTRESTClient {
 	public String getRestInterfaceBaseURL() {
 		return restInterfaceBaseURL;
 	}
+
 	public void setRestInterfaceBaseURL(String restInterfaceBaseURL) {
 		this.restInterfaceBaseURL = restInterfaceBaseURL;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
-	}	
+	}
+
 }
